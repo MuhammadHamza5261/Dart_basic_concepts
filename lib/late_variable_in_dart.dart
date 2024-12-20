f61d82a85a3419e2e11c5946e46ca72bcdbdda09
MuhammadHamza5261 <hamzafiaz708@gmail.com>
2024-12-20 23:41:03 +0500

    dart basic concepts

--- lib/late_variable_in_dart.dart
+++ lib/late_variable_in_dart.dart
@@ -2,20 +2,19 @@
  lekin uski value program ke kisi specific part mein assign karni ho.*/
 
 
-class LateVariableScreen{
+ class LateVariableScreen{
 
-  late String name;
+   late String name;
+
 
    void setName(){
      name = "Muhammad Hamza";
    }
 
+
    void printName(){
      print(name);
    }
-
-
-
 
 
 }
